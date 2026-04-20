49108b648b22b55462ae8f3534c39196eb0627ce
Nguyen Phuc Tran <phuctran5219@gmail.com>
2026-04-20 15:43:00 -0400

    Testing implementation of Graph.cpp

---
+++ AdjList.h
@@ -0,0 +1,12 @@
+#ifndef ADJLIST_H
+#define ADJLIST_H
+
+struct Node {
+
+};
+
+class AdjList {
+
+};
+
+#endif
--- Graph.h
+++ Graph.h
@@ -1,25 +1,34 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+
+#include <list>
+
 // forward declaration
 class Vertex;
 class Edge;
 
 template <typename T> class Graph {
     private:
-        
+        std::list<Vertex*> adjList; // Adjacency List
+        std::vector<Vertex*> vList;
+        std::vector<Edge*> eList;
     public:
         //   Return a vertex list of all the vertices of the graph.
-        vertices()
+        std::vector<Vertex*> vertices() {
+            return vList;
+        }
         //  Return an edge list of all the edges of the graph.
-        edges()
+        std::vector<Edge*> edges() {
+            return eList;
+        }
         //  Insert and return a new vertex storing element x
-        insertVertex(x)
+        Vertex::Vertex* insertVertex(x);
         /*  Insert and return a new undeirected edge with 
             end vertices v and w and storing element x*/
-        insertEdge(v,w,x)
+        Edge::Edge* insertEdge(v,w,x);
         //  Remove vertex v and all its incident edges
-        eraseVertex(v)
+        void eraseVertex(v);
         // Remove edge e
-        eraseEdge(e)
+        void eraseEdge(e);
 };
 #endif
