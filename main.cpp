87851a083fd8c73cefbe51091ce760b3d1e9e6d6
Ryan <--list>
2026-04-23 23:58:14 -0400

    Include GraphUI header and add startup message in main

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include "Vertex.h"
 #include "Graph.h"
 #include "GraphBuilder.h"
-
+#include "GraphUI.h"
 int main() {
-    
+    std::cout << "Hello!\n";
     Graph<std::string, double> graph = loadGraph<std::string, double>();
     
     userInteraction(graph);
